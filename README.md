234a52161b1f7cfd2fe6946341731f6d5b86c24b
Naoto Omura <naoto.omura@pixta.co.jp>
2019-09-24 17:49:33 +0900

    for multiple breadcrumbs

--- README.md
+++ README.md
@@ -181,7 +181,7 @@ end
 
 ### Restricting breadcrumb scope
 
-The `add_breadcrumb` method understands all options you are used to pass to a Rails controller filter. In fact, behind the scenes this method uses a `before_filter` to store the tab in the `@breadcrumbs` variable.
+The `add_breadcrumb` method understands all options you are used to pass to a Rails controller filter. In fact, behind the scenes this method uses a `before_filter` to store the tab in the `@breadcrumbs_list` variable.
 
 Taking advantage of Rails filter options, you can restrict a tab to a selected group of actions in the same controller.
 
--- lib/breadcrumbs_on_rails/action_controller.rb
+++ lib/breadcrumbs_on_rails/action_controller.rb
@@ -15,6 +15,7 @@ module BreadcrumbsOnRails
       extend          ClassMethods
       helper          HelperMethods
       helper_method   :add_breadcrumb, :breadcrumbs
+      helper_method   :next_breadcrumbs_list, :breadcrumbs_list
 
       unless base.respond_to?(:before_action)
         base.alias_method :before_action, :before_filter
@@ -28,7 +29,15 @@ module BreadcrumbsOnRails
     end
 
     def breadcrumbs
-      @breadcrumbs ||= []
+      self.breadcrumbs_list.last
+    end
+
+    def next_breadcrumbs_list
+      self.breadcrumbs_list << []
+    end
+
+    def breadcrumbs_list
+      @breadcrumbs_list ||= [[]]
     end
 
     module Utils
@@ -83,15 +92,17 @@ module BreadcrumbsOnRails
     module HelperMethods
 
       def render_breadcrumbs(options = {}, &block)
-        builder = (options.delete(:builder) || Breadcrumbs::SimpleBuilder).new(self, breadcrumbs, options)
-        content = builder.render.html_safe
-        if block_given?
-          capture(content, &block)
-        else
-          content
-        end
+        option_builder = options.delete(:builder)
+        breadcrumbs_list.map do |bcs|
+          builder = (option_builder || Breadcrumbs::SimpleBuilder).new(self, bcs, options)
+          content = builder.render
+          if block_given?
+            capture(content, &block)
+          else
+            content
+          end
+        end.join( options.delete(:list_separator)).html_safe
       end
-
     end
 
   end
